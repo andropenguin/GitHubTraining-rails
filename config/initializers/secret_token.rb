c784ac736a6c45671d88e61d866c098b4c8edb32
andropenguin <andropenguin@gmail.com>
2014-04-09 10:31:24 +0900

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitHubTrainingRails::Application.config.secret_key_base = 'ffcdde76387880c2d17cbe63626750cb8e5d570aa05c3ee16c88f204549626540f980ec2d71998237294530bbe9f62a8f86e884db1ce9b77830fcb7a0b5dedb0'
